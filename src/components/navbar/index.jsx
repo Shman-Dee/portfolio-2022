1cd3578f8ca9074fd8a2abe2d0f93c9d5c5db9e3
Andrew McInally <realfunnyestate@yahoo.com>
2022-06-24 14:47:30 -0600

    finished up nav bar functionality

--- src/components/navbar/index.jsx
+++ src/components/navbar/index.jsx
@@ -1,9 +1,17 @@
-import React from 'react'
+
 import { Link } from 'react-router-dom'
-import { FaReact } from 'react-icons/fa'
+import { FaReact, FaBars } from 'react-icons/fa'
+import { HiX } from 'react-icons/hi'
 import { navMenus } from './config'
+import { useState } from 'react'
+import './style.scss'
 
 function Navbar() {
+  const [click, setClick] = useState(false)
+  const handleClick = () => {
+    setClick(!click)
+  }
+
   return (
     <div>
       <nav className="navbar">
@@ -11,7 +19,13 @@ function Navbar() {
           <Link to={'/'} className="navbar__container__logo">
             <FaReact size={30} />
           </Link>
-          <ul className="navbar__container__menu">
+          <ul
+            className={
+              click
+                ? 'navbar__container__menu active'
+                : 'navbar__container__menu'
+            }
+          >
             {navMenus.map((item, key) => (
               <li key={key} className="navbar__container__menu__item">
                 <Link
@@ -23,6 +37,9 @@ function Navbar() {
               </li>
             ))}
           </ul>
+          <div className="nav-icon" onClick={handleClick}>
+            {click ? <HiX size={30} /> : <FaBars size={30} />}
+          </div>
         </div>
       </nav>
     </div>
