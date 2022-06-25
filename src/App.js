7bcb97813fc5db018bbde3bd8f2f490774c11fdf
Andrew McInally <realfunnyestate@yahoo.com>
2022-06-25 14:49:15 -0600

    added react particlesjs

--- src/App.js
+++ src/App.js
@@ -1,20 +1,31 @@
 import './App.css'
 import { Routes, Route } from 'react-router-dom'
 import './css/style.css'
-
-import Home from "./containers/home"
-import About from "./containers/about"
-import Skills from "./containers/skills"
-import Resume from "./containers/resume"
-import Portfolio from "./containers/portfolio"
-import Contact from "./containers/contact"
-import Navbar from "./components/navbar"
-
+import Particles from 'react-tsparticles'
+import { loadFull } from 'tsparticles'
+import Home from './containers/home'
+import About from './containers/about'
+import Skills from './containers/skills'
+import Resume from './containers/resume'
+import Portfolio from './containers/portfolio'
+import Contact from './containers/contact'
+import Navbar from './components/navbar'
+import particlesConfig from './helpers/particlesConfig'
 
 function App() {
+
+  const particlesInit = async (main) => {
+    await loadFull(main)
+  }
+
   return (
     <div className="App">
-      <Navbar/>
+      <Particles
+        id="particles"
+        options={particlesConfig}
+        init={particlesInit}
+      />
+      <Navbar />
       <Routes>
         <Route path="/" index element={<Home />} />
         <Route path="/about" index element={<About />} />
