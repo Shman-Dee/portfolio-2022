c74138a9942b456ef6ac1ec47f6b7abd7278f663
Andrew McInally <realfunnyestate@yahoo.com>
2022-06-25 21:14:59 -0600

    added icons

--- src/containers/home/index.jsx
+++ src/containers/home/index.jsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import {FaLinkedin, FaGithub} from 'react-icons/fa'
 import './style.scss'
 
 function Home() {
@@ -6,10 +7,20 @@ function Home() {
     <section className="home" id="home">
       <div className="home__text-wrapper">
         <h1 className="title-text">
-          Hello, I'm Andrew
+          Hello, I'm Andrew.
           <br />
-          Full Stack Developer.
+          Full Stack Developer
         </h1>
+      </div>
+      <div className="contact-me">
+        <div className="contact-me__buttons-wrapper">
+          <button>Hire Me</button>
+          <a>Download Resume</a>
+        </div>
+        <div className="contact-me__socials-wrapper">
+          <FaLinkedin size={30} />
+          <FaGithub size={30} />
+        </div>
       </div>
     </section>
   )
