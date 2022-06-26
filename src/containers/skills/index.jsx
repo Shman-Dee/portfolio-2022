0ea63ae3de46ec979c8f67d4e69fedefa94f02b6
Andrew McInally <realfunnyestate@yahoo.com>
2022-06-26 17:16:02 -0600

    working on skills with percentage bars

--- src/containers/skills/index.jsx
+++ src/containers/skills/index.jsx
@@ -1,8 +1,42 @@
 import React from 'react'
+import { skillsData } from './utils'
+import PageHeaderContent from '../../components/PageHeaderContent'
+import { GiSkills } from 'react-icons/gi'
+import { Line } from 'rc-progress'
 
 function Skills() {
   return (
-    <div>Skills</div>
+    <div>
+      <section id="skills" className="skills">
+        <PageHeaderContent
+          headerText="My Skills"
+          icon={<GiSkills size={40} />}
+        />
+        <div className="skills__content-wrapper">
+          {skillsData.map((item, key) => (
+            <div key={key} className="skills__content-wrapper__inner-content">
+              <h3 className="skills__content-wrapper__inner-content__category-text">
+                {item.label}
+              </h3>
+              <div>
+                {item.data.map((dataItem, index) => (
+                  <div className="progressbar-wrapper" key={index}>
+                    <p>{dataItem.skillName}</p>
+                    <Line
+                      percent={dataItem.percentage}
+                      strokeWidth="2"
+                      strokeColor="var(--selected-theme-main-color"
+                      strokeLinecap="square"
+                      trailWidth="2"
+                    />
+                  </div>
+                ))}
+              </div>
+            </div>
+          ))}
+        </div>
+      </section>
+    </div>
   )
 }
 
