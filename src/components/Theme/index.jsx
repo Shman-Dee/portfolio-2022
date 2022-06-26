bcfa37b3b50cbc21d19faafa33de4829ef7cb866
Andrew McInally <realfunnyestate@yahoo.com>
2022-06-25 22:09:46 -0600

    working on theme button

--- src/components/Theme/index.jsx
+++ src/components/Theme/index.jsx
@@ -1,41 +1,51 @@
 import React from 'react'
 import { ImCog } from 'react-icons/im'
 import './style.scss'
+import { useState } from 'react'
+
+const colorsArray = [
+  {
+    id: 'yellow',
+    bgColor: '#ffdd40',
+  },
+  {
+    id: 'red',
+    bgColor: '#E82A2A',
+  },
+  {
+    id: 'green',
+    bgColor: '#6ac045',
+  },
+  {
+    id: 'blue',
+    bgColor: '#5078ff',
+  },
+]
 
 function Theme() {
+  const [theme, setCurrentTheme] = useState('yellow')
+  const [toggle, setToggle] = useState(false)
 
-  const colorsArray = [
-    {
-      id : 'yellow',
-      bgColor : '#ffdd40'
-    },
-    {
-      id : 'red',
-      bgColor : '#E82A2A'
-    },
-    {
-      id : 'green',
-      bgColor : '#6ac045'
-    },
-    {
-      id : 'blue',
-      bgColor : '#5078ff'
-    },
-  ]
+  const handleToggleTheme = (currentId) => {
+    setCurrentTheme(currentId)
+    setToggle(false)
+  }
 
   return (
-    <div className="theme-wrapper">
+    <div className={`theme-wrapper ${toggle ? 'active' : ''}`}>
       <div className="them-wrapper__toggle-icon">
-        <ImCog size={40}/>
+        <ImCog onClick={() => setToggle(!toggle)} size={40} />
       </div>
       <div className="theme-wrapper__menu">
         <h4>Choose Theme</h4>
         <ul>
-          {
-            colorsArray.map((item, key) => (
-              <li key={key} style={{background : item.bgColor}}/>
-            ))
-          }
+          {colorsArray.map((item, key) => (
+            <li
+              onClick={() => handleToggleTheme(item.id)}
+              key={key}
+              style={{ background: item.bgColor }}
+            />
+          ))}
         </ul>
       </div>
     </div>
