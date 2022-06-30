033c8f8ea741b1637627f4b37030fbefd14309f9
Andrew McInally <realfunnyestate@yahoo.com>
2022-06-30 15:30:47 -0600

    started adding projects to portfolio page

--- src/containers/portfolio/index.jsx
+++ src/containers/portfolio/index.jsx
@@ -1,8 +1,34 @@
 import React from 'react'
+import { AiFillProject } from 'react-icons/ai'
+import PageHeaderContent from '../../components/PageHeaderContent'
+import { filterOptions, portfolioData } from './utils'
 
 function Portfolio() {
   return (
-    <div>Portfolio</div>
+    <section className="portfolio" id="portfolio">
+      <PageHeaderContent
+        headerText="My Portfolio"
+        icon={<AiFillProject size={40} />}
+      />
+      <div className="portfolio__content">
+        <ul className="portfolio__content__filter">
+          {filterOptions.map((option, key) => (
+            <li key={option.id}>{option.label}</li>
+          ))}
+        </ul>
+        <div className="portfolio__content__cards">
+          {portfolioData.map((item) => (
+            <div className="portfolio__content__cards__item">
+              <div className="portfolio__content__cards__item__img-wrapper">
+                <a>
+                  <img src={item.image} alt="project image" />
+                </a>
+              </div>
+            </div>
+          ))}
+        </div>
+      </div>
+    </section>
   )
 }
 
---
+++ src/containers/portfolio/utils.js
@@ -0,0 +1,39 @@
+import ImageOne from '../../images/cryptococktail.jpg'
+import ImageTwo from '../../images/promangsyst.jpg'
+import ImageThree from '../../images/codeinmind.jpg'
+
+export const portfolioData = [
+  {
+    sectionId: 2,
+    projectName: 'Crypto Cocktails',
+    projectLink: 'google.com',
+    image: ImageOne,
+  },
+  {
+    sectionId: 3,
+    projectName: 'Project Management System',
+    projectLink: 'google.com',
+    image: ImageTwo,
+  },
+  {
+    sectionId: 2,
+    projectName: 'Code In Mind',
+    projectLink: 'google.com',
+    image: ImageThree,
+  },
+]
+
+export const filterOptions = [
+  {
+    label: 'All',
+    id: 1,
+  },
+  {
+    label: 'Development',
+    id: 2,
+  },
+  {
+    label: 'Design',
+    id: 3,
+  },
+]
