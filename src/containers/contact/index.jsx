add5c8af68c7e647687da833573d2b38d92317aa
Andrew McInally <realfunnyestate@yahoo.com>
2022-06-30 17:51:47 -0600

    styled contact page

--- src/containers/contact/index.jsx
+++ src/containers/contact/index.jsx
@@ -1,8 +1,44 @@
 import React from 'react'
+import PageHeaderContent from '../../components/PageHeaderContent'
+import { RiContactsFill } from 'react-icons/ri'
 
 function Contact() {
   return (
-    <div>Contact</div>
+    <section id="contact" className="contact">
+      <PageHeaderContent
+        headerText="Contact Me"
+        icon={<RiContactsFill size={40} />}
+      />
+      <div className="contact__content">
+        <h3 className="contact__content__header-text">Let's Talk</h3>
+        <div className="contact__content__form">
+          <div className="nameWrapper">
+            <input name="name" className="inputName" type="text" />
+            <label htmlFor="name" className="namelabel">
+              Name
+            </label>
+          </div>
+          <div className="emailWrapper">
+            <input name="email" className="inputEmail" type="text" />
+            <label htmlFor="email" className="emaillabel">
+              Email
+            </label>
+          </div>
+          <div className="descriptionWrapper">
+            <textarea
+              name="description"
+              className="inputDescription"
+              type="text"
+              rows={'5'}
+              style={{ resize: 'none' }}
+            />
+            <label htmlFor="description" className="descriptionlabel">
+              Description
+            </label>
+          </div>
+        </div>
+      </div>
+    </section>
   )
 }
 
