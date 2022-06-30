42ab1ef16d03a74cb5eb80b271d6642b8ed8c519
Andrew McInally <realfunnyestate@yahoo.com>
2022-06-30 12:03:27 -0600

    finished styling resume page

--- src/containers/resume/index.jsx
+++ src/containers/resume/index.jsx
@@ -8,6 +8,7 @@ import {
 } from 'react-vertical-timeline-component'
 import 'react-vertical-timeline-component/style.min.css'
 import { data } from './utils'
+import './style.scss'
 
 function Resume() {
   return (
@@ -38,22 +39,51 @@ function Resume() {
                   color: 'var(--selected-theme-main-color)',
                 }}
                 icon={<MdWork size={40} />}
-                dateClassName="vertical-timeline-element-custom-date"
               >
                 <div className="vertical-timeline-element-title-wrapper">
-                  <h3 className="vertical-timeline-element-title">
-                    {item.title}
-                  </h3>
-                  <h4 className="vertical-timeline-element-subtitle">
-                    {item.subTitle}
-                  </h4>
+                  <h3>{item.title}</h3>
+                  <h4>{item.subTitle}</h4>
                 </div>
-                <p>{item.description}</p>
+                <p className="vertical-timeline-element-description-wrapper">
+                  {item.description}
+                </p>
               </VerticalTimelineElement>
             ))}
           </VerticalTimeline>
         </div>
-        <div className="education"></div>
+        <div className="timeline__education">
+          <h3 className="timeline__education__header-text">Education</h3>
+          <VerticalTimeline
+            layout="1-column"
+            lineColor="var(--selected-theme-main-color)"
+          >
+            {data.education.map((item) => (
+              <VerticalTimelineElement
+                key={item.title}
+                className="timeline__education__vertical-timeline-component"
+                contentStyle={{
+                  background: 'none',
+                  color: 'var(--selected-theme-main-color',
+                  border: '1.5px solid var(--selected-theme-main-color)',
+                }}
+                date="2021 - present"
+                iconStyle={{
+                  background: '#181818',
+                  color: 'var(--selected-theme-main-color)',
+                }}
+                icon={<MdWork size={40} />}
+              >
+                <div className="vertical-timeline-element-title-wrapper">
+                  <h3>{item.title}</h3>
+                  <h4>{item.subTitle}</h4>
+                </div>
+                <p className="vertical-timeline-element-description-wrapper">
+                  {item.description}
+                </p>
+              </VerticalTimelineElement>
+            ))}
+          </VerticalTimeline>
+        </div>
       </div>
     </section>
   )
