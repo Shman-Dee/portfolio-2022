e13e4bca93875cb9bec4d483da258f301aa761c8
Andrew McInally <realfunnyestate@yahoo.com>
2022-07-01 23:42:24 -0600

    love dad

--- src/containers/contact/index.jsx
+++ src/containers/contact/index.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import PageHeaderContent from '../../components/PageHeaderContent'
 import { RiContactsFill } from 'react-icons/ri'
+import './style.scss'
 
 function Contact() {
   return (
@@ -12,29 +13,31 @@ function Contact() {
       <div className="contact__content">
         <h3 className="contact__content__header-text">Let's Talk</h3>
         <div className="contact__content__form">
-          <div className="nameWrapper">
-            <input name="name" className="inputName" type="text" />
-            <label htmlFor="name" className="namelabel">
-              Name
-            </label>
-          </div>
-          <div className="emailWrapper">
-            <input name="email" className="inputEmail" type="text" />
-            <label htmlFor="email" className="emaillabel">
-              Email
-            </label>
-          </div>
-          <div className="descriptionWrapper">
-            <textarea
-              name="description"
-              className="inputDescription"
-              type="text"
-              rows={'5'}
-              style={{ resize: 'none' }}
-            />
-            <label htmlFor="description" className="descriptionlabel">
-              Description
-            </label>
+          <div className="contact__content__form__controlsWrapper">
+            <div className="nameWrapper">
+              <input name="name" className="inputName" type="text" />
+              <label htmlFor="name" className="namelabel">
+                Name
+              </label>
+            </div>
+            <div className="emailWrapper">
+              <input name="email" className="inputEmail" type="text" />
+              <label htmlFor="email" className="emaillabel">
+                Email
+              </label>
+            </div>
+            <div className="descriptionWrapper">
+              <textarea
+                name="description"
+                className="inputDescription"
+                type="text"
+                rows={'5'}
+                style={{ resize: 'none' }}
+              />
+              <label htmlFor="description" className="descriptionlabel">
+                Description
+              </label>
+            </div>
           </div>
         </div>
       </div>
